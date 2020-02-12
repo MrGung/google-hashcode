d3f0103be1184fc75677048075aac2d1671b24c5
Steffen Glückselig <gist@gungfu.de>
2020-02-12 17:18:57 +0100

    Create grid (of rides).

--- src/hashcode2018/core.clj
+++ src/hashcode2018/core.clj
@@ -7,6 +7,8 @@
   (println "Hello, World!"))
 
 
+
+;; ## parse input
 (defn parse-line-to-integers [line]
   (-> line
       (s/split #" ")
@@ -23,7 +25,6 @@
        (zipmap
         [:#row-start :#column-start :#row-finish :#column-finish :#earliest-start :#latest-finish])))
 
-
 (defn parse-input [filename]
   (with-open [rdr (clojure.java.io/reader
                    (.getPath (clojure.java.io/resource filename)))]
@@ -34,6 +35,24 @@
        :rides  rides})))
 
 
+;; ## build structures
+(defn assign-rides-to-grid [rides]
+  (reduce
+   (fn [grid ride]
+     (let [coord [((juxt :#row-start :#column-start) ride)]]
+       (println coord)
+       (update-in grid
+                  coord
+                  conj
+                  ride)))
+   {}
+   rides))
+
+
+
+
 (let [{:keys [header rides]} (parse-input "hashcode2018/a_example.in")]
-  (clojure.pprint/pprint header)
-  (clojure.pprint/pprint rides))
+  ;  (clojure.pprint/pprint header)
+  ;  (clojure.pprint/pprint rides)
+  (let [grid (assign-rides-to-grid rides)]
+    (clojure.pprint/pprint grid)))
